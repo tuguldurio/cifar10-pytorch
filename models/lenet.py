1c55ed72a5d50dae337a11a6f5a26e4b0a578629
tuguldurio <ok.tuguldur@gmail.com>
2020-11-19 13:47:11 -0500

    detailed input size

--- models/lenet.py
+++ models/lenet.py
@@ -6,7 +6,7 @@ class LeNet(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(400, 120)
+        self.fc1 = nn.Linear(16*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
     
