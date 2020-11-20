811f969e0652a735ab8f1b977f9ffe4197e977d0
tuguldurio <ok.tuguldur@gmail.com>
2020-11-20 17:03:19 -0500

    train function, minimal prediction tester

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import numpy as np
 import matplotlib.pyplot as plt
-
+import numpy as np
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+
 from models.lenet import LeNet
+
+EPOCHS = 10
 
 # Load data
 transform = transforms.Compose(
@@ -13,7 +17,7 @@ transform = transforms.Compose(
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=False, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
                                           shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
@@ -21,8 +25,39 @@ testset = torchvision.datasets.CIFAR10(root='./data', train=False,
 testloader = torch.utils.data.DataLoader(testset, batch_size=100,
                                          shuffle=False, num_workers=2)
 
-img,_ = iter(trainloader).next()
-
+# Define model, loss function and optimizers
 model = LeNet()
-pred = model(img)
-print(pred)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# Train
+def train():
+    for epoch in range(EPOCHS):
+        running_loss = 0.0
+        for i, (x, y) in enumerate(trainloader):
+            optimizer.zero_grad()
+
+            pred = model(x)
+            loss = criterion(pred, y)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print(f'epoch [{epoch+1}/{EPOCHS}], loss: {running_loss/len(trainloader):.3f}')
+        running_loss = 0.0
+
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+print('GroundTruth: ', ' '.join('%5s' % testset.classes[labels[j]] for j in range(4)))
+
+outputs = model(images)
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % testset.classes[predicted[j]]
+                              for j in range(4)))
+
+if __name__ == '__main__':
+    train()
