cf3b8acc0c4a393a03d1d4a35cc5b1c2ea3b392d
tuguldurio <ok.tuguldur@gmail.com>
2020-12-01 19:31:01 -0500

    arg name

--- models/lenet.py
+++ models/lenet.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 class LeNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
         self.fc1 = nn.Linear(16*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
