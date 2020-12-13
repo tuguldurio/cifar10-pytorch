ad73cb9912bc3291be4f67452994aa23461479fd
tuguldurio <ok.tuguldur@gmail.com>
2020-12-13 16:35:48 -0500

    utils loads data

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
+import utils
 import models
 
 # Train
@@ -68,19 +67,7 @@ def main():
     args = parser.parse_args()
     
     # Load data
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
-                                            shuffle=True, num_workers=2)
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                        download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
-                                            shuffle=True, num_workers=2)
+    train_loader, test_loader = utils.load_data(args)
 
     # cuda or cpu
     if torch.cuda.is_available():
---
+++ utils.py
@@ -0,0 +1,19 @@
+import torchvision
+import torchvision.transforms as transforms
+
+def load_data(args):
+    transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+                                            shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
+                                                shuffle=True, num_workers=2)
+
+    return trainloader, testloader
