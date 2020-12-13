a39e8794e09b025103de4356017faacee0867915
tuguldurio <ok.tuguldur@gmail.com>
2020-12-13 16:43:59 -0500

    minor fix

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-import torchvision
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
 import torchvision.transforms as transforms
 
 def load_data(args):
@@ -6,14 +7,12 @@ def load_data(args):
             [transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
-                                            shuffle=True, num_workers=2)
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    trainset = CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
-                                                shuffle=True, num_workers=2)
+    trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+    testset = CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform)
+    testloader = DataLoader(testset, batch_size=100, shuffle=True, num_workers=2)
 
     return trainloader, testloader
