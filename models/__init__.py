e06caf0c2bb7f7f17a755f43c8a77fcb740ebd7c
tuguldurio <ok.tuguldur@gmail.com>
2020-12-13 16:35:54 -0500

    densenet

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 from .lenet import LeNet
 from .vgg import VGG
 from .alexnet import AlexNet
+from .densenet import DenseNet, Bottleneck
 
+# VGG
 class VGG11(VGG):
     def __init__(self):
         super().__init__('VGG11')
@@ -18,11 +20,34 @@ class VGG19(VGG):
     def __init__(self):
         super().__init__('VGG19')
 
+
+# Dense Net
+class DenseNet121(DenseNet):
+    def __init__(self):
+        super().__init__(Bottleneck, [6, 12, 24, 16], growth_rate=32)
+
+class DenseNet169(DenseNet):
+    def __init__(self):
+        super().__init__(Bottleneck, [6, 12, 32, 32], growth_rate=32)
+
+class DenseNet201(DenseNet):
+    def __init__(self):
+        super().__init__(Bottleneck, [6, 12, 48, 32], growth_rate=32)
+
+class DenseNet161(DenseNet):
+    def __init__(self):
+        super().__init__(Bottleneck, [6, 12, 36, 24], growth_rate=48)
+
+class DenseNet121(DenseNet):
+    def __init__(self):
+        super().__init__(Bottleneck, [6, 12, 24, 16], growth_rate=12)
+
 names = {
     'lenet': LeNet,
     'vgg11': VGG11,
     'vgg13': VGG13,
     'vgg16': VGG16,
     'vgg19': VGG19,
-    'alexnet': AlexNet
+    'alexnet': AlexNet,
+    'densenet121': DenseNet121,
 }
--- models/densenet.py
+++ models/densenet.py
@@ -1,6 +1,83 @@
-import torch.nn  as nn
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_planes, growth_rate):
+        super().__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, 4*growth_rate, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(4*growth_rate)
+        self.conv2 = nn.Conv2d(4*growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
+
+    def forward(self, x):
+        x_ = x
+        x = self.conv1(F.relu(self.bn1(x)))
+        x = self.conv2(F.relu(self.bn2(x)))
+        x = torch.cat([x,x_], 1)
+        return x
+
+
+class Transition(nn.Module):
+    def __init__(self, in_planes, out_planes):
+        super().__init__()
+        self.bn = nn.BatchNorm2d(in_planes)
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
+
+    def forward(self, x):
+        x = self.conv(F.relu(self.bn(x)))
+        x = F.avg_pool2d(x, 2)
+        return x
+
 
 class DenseNet(nn.Module):
-    def __init__(self):
+    def __init__(self, block, nblocks, growth_rate=12, reduction=0.5, num_classes=10):
         super().__init__()
-        
+        self.growth_rate = growth_rate
+
+        num_planes = 2*growth_rate
+        self.conv1 = nn.Conv2d(3, num_planes, kernel_size=3, padding=1, bias=False)
+
+        self.dense1 = self._make_dense_layers(block, num_planes, nblocks[0])
+        num_planes += nblocks[0]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans1 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense2 = self._make_dense_layers(block, num_planes, nblocks[1])
+        num_planes += nblocks[1]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans2 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense3 = self._make_dense_layers(block, num_planes, nblocks[2])
+        num_planes += nblocks[2]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans3 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense4 = self._make_dense_layers(block, num_planes, nblocks[3])
+        num_planes += nblocks[3]*growth_rate
+
+        self.bn = nn.BatchNorm2d(num_planes)
+        self.linear = nn.Linear(num_planes, num_classes)
+
+    def _make_dense_layers(self, block, in_planes, nblock):
+        layers = []
+        for i in range(nblock):
+            layers.append(block(in_planes, self.growth_rate))
+            in_planes += self.growth_rate
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.trans1(self.dense1(x))
+        x = self.trans2(self.dense2(x))
+        x = self.trans3(self.dense3(x))
+        x = self.dense4(x)
+        x = F.avg_pool2d(F.relu(self.bn(x)), 4)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        return x
